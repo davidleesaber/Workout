b373253cdedc10f56ef2bd0f94e83764191825e1
davidleesaber <davidl.lee@mail.utoronto.ca>
2025-07-14 18:40:26 -0400

    set up expo router

--- src/app/index.js
+++ src/app/index.js
@@ -1,7 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, FlatList } from 'react-native';
-import exercises from './assets/data/exercises.json';
-import ExerciseListItem from './src/components/ExerciseListItem.jsx';
+import exercises from '../../assets/data/exercises.json';
+import ExerciseListItem from '../../src/components/ExerciseListItem.jsx';
 
 export default function App() {
 
@@ -23,10 +23,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'gainsboro',
+    backgroundColor: 'ghostwhite',
     justifyContent: 'center',
     padding: 10,
-    marginTop: 70,
   }
 })
 
--- src/components/ExerciseListItem.jsx
+++ src/components/ExerciseListItem.jsx
@@ -1,36 +1,43 @@
+import { StyleSheet, Text, View } from "react-native";
 
-import { StyleSheet, Text, View } from 'react-native';
-
-
-export default function ExerciseListItem({item}) {
+export default function ExerciseListItem({ item }) {
   return (
-          <View style={styles.exerciseContainer}>
-              <Text style={styles.exerciseName}>{item.name}</Text>
-              <Text style={styles.exerciseSubtitle}>
-                <Text style={styles.subVal}>{item.muscle}</Text> | {' '}
-                <Text style={styles.subVal}>{item.equipment}</Text>
-              </Text>
-            </View>
+    <View style={styles.exerciseContainer}>
+      <Text style={styles.exerciseName}>{item.name}</Text>
+      <Text style={styles.exerciseSubtitle}>
+        <Text style={styles.subVal}>{item.muscle}</Text> |{' '}
+        <Text style={styles.subVal}>{item.equipment}</Text>
+      </Text>
+    </View>
   );
 }
 
-
 const styles = StyleSheet.create({
-  exerciseContainer: {
-    backgroundColor: '#fff',
-    padding: 10,
-    borderRadius: 10,
-    gap: 5,
-  },
-  exerciseName: {
-    fontSize: 20, 
-    fontWeight: '500', 
-  },
-  exerciseSubtitle: {
-    color: 'dimgray',
-  },
-  subVal:
-  {
-    textTransform: 'capitalize',
-  }
-})
+    exerciseContainer: {
+        backgroundColor: "#fff",
+        padding: 10,
+        borderRadius: 10,
+        gap: 5,
+        marginHorizontal: 2,
+
+        
+        // shadow
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 1,
+        },
+        shadowOpacity: 0.2,
+        shadowRadius: 1.41, 
+    },
+    exerciseName: {
+        fontSize: 20,
+        fontWeight: "500",
+    },
+    exerciseSubtitle: {
+        color: "dimgray",
+    },
+    subVal: {
+        textTransform: "capitalize",
+    },
+    });
